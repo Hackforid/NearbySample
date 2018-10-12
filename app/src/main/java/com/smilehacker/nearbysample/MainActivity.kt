e7a6ebe909c281e4e42b1fd66366377d3bf11f5b
Sai <zhouquanbest@gmail.com>
2018-10-12 19:25:51 +0800

    add test case

--- app/src/main/java/com/smilehacker/nearbysample/MainActivity.kt
+++ app/src/main/java/com/smilehacker/nearbysample/MainActivity.kt
@@ -1,48 +1,82 @@
 package com.smilehacker.nearbysample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.connection.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val mBtnRua by lazy { findViewById<Button>(R.id.btn_rua) }
+    private val mEtName by lazy { findViewById<EditText>(R.id.et_name) }
+    private val mTvIds by lazy { findViewById<TextView>(R.id.tv_ids) }
+    private val mConnClient : ConnectionsClient by lazy { Nearby.getConnectionsClient(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mBtnRua.setOnClickListener {
+            val name = mEtName.text.toString()
+            discovery(name)
+        }
+
     }
 
-    private fun foo() {
-        Nearby.getConnectionsClient(this)
-                .startAdvertising("Device A", "com.smilehacker.nearbysample",
+    private fun discovery(name: String) {
+        mConnClient
+                .startAdvertising(name, "com.smilehacker.nearbysample",
                         object: ConnectionLifecycleCallback() {
-                            override fun onConnectionResult(p0: String, p1: ConnectionResolution) {
-                                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                            override fun onConnectionResult(endpointId: String, info: ConnectionResolution) {
                             }
 
-                            override fun onDisconnected(p0: String) {
-                                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                            override fun onDisconnected(endpointId: String) {
                             }
 
-                            override fun onConnectionInitiated(p0: String, p1: ConnectionInfo) {
-                                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                            override fun onConnectionInitiated(endpointId: String, info: ConnectionInfo) {
                             }
 
                         },
-                        AdvertisingOptions(Strategy.P2P_CLUSTER))
+                        AdvertisingOptions.Builder().setStrategy(Strategy.P2P_STAR).build())
+                .addOnSuccessListener {
+                    Log.i("xx", "start advertising succ")
+                }
+                .addOnFailureListener {
+                    e -> Log.e("xx", "start advertising fail", e)
+                }
+        mConnClient
+            .startDiscovery("com.smilehacker.nearbysample",
+            object: EndpointDiscoveryCallback() {
+                @SuppressLint("SetTextI18n")
+                override fun onEndpointFound(endpointId: String, info: DiscoveredEndpointInfo) {
+                    Log.i("xx", "endpointId=$endpointId name=${info.endpointName} serviceId=${info.serviceId}")
+                    mTvIds.post {
+                        mTvIds.text = "${mTvIds.text}\n${info.endpointName}"
+                    }
+                }
 
-        Nearby.getConnectionsClient(this)
-                .startDiscovery("Device B",
-                        object: EndpointDiscoveryCallback() {
-                            override fun onEndpointFound(p0: String, p1: DiscoveredEndpointInfo) {
-                                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                            }
+                override fun onEndpointLost(endpointId: String) {
+                    Log.i("xx", "endpoint lost $endpointId")
+                }
 
-                            override fun onEndpointLost(p0: String) {
-                                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                            }
+            },
+                    DiscoveryOptions.Builder().setStrategy(Strategy.P2P_STAR).build())
+            .addOnSuccessListener {
+                Log.i("", "start discovery succ")
+            }
+            .addOnFailureListener {
+                e -> Log.e("", "start discovery fail", e)
+            }
+    }
 
-                        },
-                        DiscoveryOptions(Strategy.P2P_CLUSTER))
+    override fun onDestroy() {
+        super.onDestroy()
+        mConnClient.stopAdvertising()
+        mConnClient.stopDiscovery()
     }
 }
